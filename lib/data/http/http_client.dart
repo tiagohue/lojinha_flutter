bce15a59874f8e8a855855313eee083a1fcfb7ff
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-06-25 11:46:31 -0300

    Adição dos metodos post e delete
    No http client

--- lib/data/http/http_client.dart
+++ lib/data/http/http_client.dart
@@ -2,6 +2,10 @@ import "package:http/http.dart" as http;
 
 abstract class IHttpClient {
   Future get({required String url});
+
+  Future post({required String url, required String body});
+
+  Future delete({required String url, required String id});
 }
 
 class HttpClient extends IHttpClient {
@@ -10,5 +14,23 @@ class HttpClient extends IHttpClient {
   @override
   Future get({required String url}) async {
     return await client.get(Uri.parse(url));
+  }
+
+  @override
+  Future post({required String url, required String body}) {
+    return client.post(
+      Uri.parse(url),
+      body: body,
+      headers: {"Content-Type": "application/json"},
+    );
+  }
+
+  @override
+  Future delete({required String url, required String id}) async {
+    final response = await http.delete(Uri.parse("$url/$id"));
+
+    if (response.statusCode != 200) {
+      throw Exception("Erro ao deletar pedido...");
+    }
   }
 }
