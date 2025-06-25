baa2f49df614384d59f65ac81e95ac72b6dde39a
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-06-25 11:44:45 -0300

    Ajuste no produto_model

--- lib/data/models/produto_model.dart
+++ lib/data/models/produto_model.dart
@@ -1,22 +1,34 @@
 class ProdutoModel {
+  String? id;
   final String title;
   final double price;
   final String category;
-  final String image;
+  final String description;
 
   ProdutoModel({
+    this.id,
     required this.title,
     required this.price,
     required this.category,
-    required this.image,
+    required this.description,
   });
 
   factory ProdutoModel.fromMap(Map<String, dynamic> map) {
     return ProdutoModel(
+      id: map["id"],
       title: map["title"],
       price: map["price"] * 1.0,
       category: map["category"],
-      image: map["image"],
+      description: map["description"],
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "title": title,
+      "price": price,
+      "description": description,
+      "category": category,
+    };
   }
 }
