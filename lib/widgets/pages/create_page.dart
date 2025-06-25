6b7c93228055852f05abca2e17d12bfe6215e299
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-06-25 11:49:17 -0300

    Adição da página de criação
    Apenas um layout básico, mas funcional

--- lib/widgets/pages/create_page.dart
+++ lib/widgets/pages/create_page.dart
@@ -13,7 +13,7 @@ class _CreatePageState extends State<CreatePage> {
   final titleController = TextEditingController();
   final priceController = TextEditingController();
   final categoryController = TextEditingController();
-  final imageController = TextEditingController();
+  final descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +22,7 @@ class _CreatePageState extends State<CreatePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Adicionar Produto"),
+        title: Text("Criar Produto"),
       ),
       body: Padding(
         padding: EdgeInsetsGeometry.all(15),
@@ -38,11 +38,27 @@ class _CreatePageState extends State<CreatePage> {
               decoration: const InputDecoration(labelText: 'Preço'),
               keyboardType: TextInputType.number,
             ),
+            TextFormField(
+              controller: categoryController,
+              decoration: const InputDecoration(labelText: 'Categoria'),
+            ),
+            TextFormField(
+              controller: descriptionController,
+              decoration: const InputDecoration(labelText: 'Descrição'),
+              keyboardType: TextInputType.number,
+            ),
             ElevatedButton(
               onPressed: () {
-                debugPrint("Salvar");
+                provider.criarProduto(
+                  titleController.text,
+                  double.parse(priceController.text),
+                  categoryController.text,
+                  descriptionController.text,
+                );
+
+                Navigator.pop(context);
               },
-              child: const Text('Salvar Produto'),
+              child: const Text('Criar Produto'),
             ),
           ],
         ),
